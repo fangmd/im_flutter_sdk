747271abdac78198b10a2d70ea81f74dde2564fd
fangmingdong <fangmingdong0224@gmail.com>
2020-03-16 19:55:47 +0800

    fix androidx

--- example/android/app/src/main/java/com/easemob/im_flutter_sdk_example/runtimepermissions/PermissionsManager.java
+++ example/android/app/src/main/java/com/easemob/im_flutter_sdk_example/runtimepermissions/PermissionsManager.java
@@ -19,7 +19,7 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
